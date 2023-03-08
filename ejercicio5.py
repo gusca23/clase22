bdd4b4122e8f531ea6f3d13b5290332fb40c9261
Gustavo <gcasaccia23@gmail.com>
2023-03-08 20:21:12 -0300

    solucion del ejercicio 5

---
+++ ejercicio5.py
@@ -0,0 +1,7 @@
+'''
+5- Calcular el nuevo salario de un obrero si obtuvo un incremento
+del 25% sobre su salario anterior
+'''
+salario = int(input('Ingrese su Salario'))
+salario_nuevo = int((salario * 0.25)) + salario
+print(salario_nuevo)
