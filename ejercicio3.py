ac2ff9aa86050af98768a5344b352ba8d127a1ec
Gustavo <gcasaccia23@gmail.com>
2023-03-08 20:05:29 -0300

    solucion de los ejercicios

--- ejercicio3.py
+++ ejercicio3.py
@@ -2,7 +2,7 @@
 3- Un vendedor recibe un sueldo base más un 10% extra por comisión de sus ventas, el vendedor desea saber cuánto dinero obtendrá por concepto de comisiones por las tres ventas que realiza en el mes y el total que recibirá en el mes tomando en cuenta su sueldo base más comisiones.
 '''
 sueldo_base = int(input('Ingrese su sueldo'))
-comision_por_ventas = (sueldo_base * 0.1) * 3
+comision_por_ventas = int((sueldo_base * 0.1) * 3)
 sueldo_mas_comision = sueldo_base + comision_por_ventas
 
 print(f'la comision por sus tres ventas es de {comision_por_ventas}gs y el total a cobrar sera de {sueldo_mas_comision}gs')
---
+++ ejercicio4.py
@@ -0,0 +1,8 @@
+'''
+4- Una tienda ofrece un descuento del 15% sobre el total de la
+compra, y un cliente desea saber cuánto deberá pagar finalmente
+sobre su compra.
+'''
+total_a_pagar = int(input('ingrese el total a pagar'))
+descuento = total_a_pagar * 0.85
+print(int(descuento))
